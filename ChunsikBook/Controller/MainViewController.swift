bc28f5daa5986ebe7dee2d9a608f64d3fd5a2dc8
Lee-Tae-Yun <cmskzkdl3698@naver.com>
2025-06-19 11:30:02 +0900

    Controller에서 View를 생성해서 View를 띄워주던 코드를controller에서 데이터를 넘겨줘서 view에서 생성하게 코드 수정

--- ChunsikBook/Controller/MainViewController.swift
+++ ChunsikBook/Controller/MainViewController.swift
@@ -13,6 +13,7 @@ class MainViewController: UIViewController {
     private var mainView = MainView()
     
     var series: Int = 1
+    var isExpanded: Bool = true
     
     override func loadView() {
         self.view = mainView
@@ -32,25 +33,14 @@ class MainViewController: UIViewController {
                 // UI 변경은 반드시 메인 스레드에서 이루어져야 하므로,
                 // 혹시 모를 백그라운드 스레드 실행에 대비해 메인 스레드로 작업을 넘기는 코드
                 DispatchQueue.main.async {
-                    self.mainView.booktitleLabel.text = books[self.series].title
-                    self.mainView.seriesButton.setTitle("\(self.series + 1)", for: .normal)
-                    self.mainView.bookimageView.image = UIImage(named: "harrypotter\(self.series + 1)")
-                    self.mainView.titleLabel.text = books[self.series].title
-                    self.mainView.authorLabel.text = books[self.series].author
-                    self.mainView.releasedLabel.text = self.formatDate(books[self.series].release_date)
-                    self.mainView.pageLabel.text = String(books[self.series].pages)
-                    self.mainView.dedicationLabel.text = books[self.series].dedication
-                    self.mainView.summeryLabel.text = books[self.series].summary
+                    let formattedDate = self.formatDate(books[self.series].release_date)
                     
-                    for chapter in books[self.series].chapters {
-                        let chapterLabel = UILabel()
-                        chapterLabel.text = "\(chapter.title)"
-                        chapterLabel.font = .systemFont(ofSize: 14)
-                        chapterLabel.textColor = .darkGray
-                        chapterLabel.numberOfLines = 0
-                        self.mainView.chaptersStackView.addArrangedSubview(chapterLabel)
-                    }
-                    
+                    self.mainView.configure(
+                        with: books[self.series],
+                        series: self.series,
+                        isExpanded: self.isExpanded,
+                        formattedDate: formattedDate
+                    )
                 }
                 
             case .failure(let error):
@@ -74,4 +64,6 @@ class MainViewController: UIViewController {
         }
         return raw
     }
+    
+    
 }
